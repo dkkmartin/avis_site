856b38a2f0cdc9d4fcd83bf4de3b65124b559ab2
martin <martinbruun1995@gmail.com>
2023-03-24 10:22:30 +0100

    Add: navbar animation

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const menuBtn = document.querySelector('.fa-bars')
 const modal = document.querySelector('.modal')
-const mobileHeader = document.querySelector('.header__mobile')
 
 menuBtn.addEventListener('click', () => {
   modal.classList.toggle('modal--active')
-  mobileHeader.classList.toggle('header--expand')
   
 })
