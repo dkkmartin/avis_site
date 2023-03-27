d94a6d56b71b0443ff217096d08af7d9d5fe9127
martin <martinbruun1995@gmail.com>
2023-03-27 14:34:21 +0200

    Add: Form validation

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const menuBtnOpen = document.querySelector('.fa-solid')
 const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 
+
 function delay(time) {
   return new Promise(resolve => setTimeout(resolve, time))
 }
+
+
 
 menuBtnOpen.addEventListener('click', () => {
   if(modal.classList.contains('modal--active')) {
@@ -29,3 +32,4 @@ menuBtnOpen.addEventListener('click', () => {
     menuBtnOpen.classList.add('fa-xmark')
   }
 })
+
